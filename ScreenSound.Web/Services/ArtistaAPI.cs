68ce32870c37cb5e3cadb72eae715a3c8c3aac70
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2025-09-24 17:08:54 -0300

    Add MusicaAPI service and expand ArtistaAPI methods
    Introduced MusicaAPI for fetching music data and enhanced ArtistaAPI with methods for adding, updating, deleting, and retrieving artists by name. Also added new image assets for artist cards and favicon.

--- ScreenSound.Web/Services/ArtistaAPI.cs
+++ ScreenSound.Web/Services/ArtistaAPI.cs
@@ -1,4 +1,5 @@
-﻿using ScreenSound.Web.Response;
+﻿using ScreenSound.Web.Requests;
+using ScreenSound.Web.Response;
 using System.Net.Http.Json;
 
 namespace ScreenSound.Web.Services;
@@ -6,6 +7,7 @@ namespace ScreenSound.Web.Services;
 public class ArtistaAPI
 {
     private readonly HttpClient _httpClient;
+
     public ArtistaAPI(IHttpClientFactory factory)
     {
         _httpClient = factory.CreateClient("API");
@@ -13,8 +15,25 @@ public class ArtistaAPI
 
     public async Task<ICollection<ArtistaResponse>?> GetArtistasAsync()
     {
-        return await
-        _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
-    }   
+        return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
+    }
 
+    public async Task AddArtistaAsync(ArtistaRequest artista)
+    {
+        await _httpClient.PostAsJsonAsync("artistas", artista);
+    }
+
+    public async Task<ArtistaResponse?> GetArtistaPorNomeAsync(string nome)
+    {
+        return await _httpClient.GetFromJsonAsync<ArtistaResponse>($"artistas/{nome}");
+    }
+    public async Task DeleteArtistaAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"artistas/{id}");
+    }
+
+    public async Task UpdateArtistaAsync(ArtistaRequestEdit artista)
+    {        
+       await _httpClient.PutAsJsonAsync($"artistas", artista);
+    }
 }
