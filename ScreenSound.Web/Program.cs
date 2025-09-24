2a7d2c1d568617178dc4dd6d08d6b4e8c4283f28
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2025-09-24 16:39:18 -0300

    Integrate MudBlazor UI library across solution
    Added MudBlazor package references to all projects and updated MainLayout.razor to use MudBlazor components for layout. Registered MudBlazor services in Program.cs, updated _Imports.razor for MudBlazor namespaces, and modified index.html to include MudBlazor CSS and JS. This enables consistent UI styling and component usage throughout the application.

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using ScreenSound.Web;
 using ScreenSound.Web.Services;
 
@@ -8,6 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddTransient<ArtistaAPI>();
+
+builder.Services.AddMudServices();
 
 builder.Services.AddHttpClient("API", client =>
 {
